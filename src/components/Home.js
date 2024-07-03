bac66b6e160cea4d71a9f2e9d29fcc255fa2b5b0
AdityaBansal1402 <bansaladitya3009@gmail.com>
2024-07-03 10:18:28 +0530

    hello

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,37 +1,63 @@
-import React, { useContext, useEffect, useState } from 'react'
-import Prompt from './Prompt'
+import React, { useContext, useEffect, useState } from 'react';
+import Prompt from './Prompt';
 import AiContext from '../AIContext';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { marked } from 'marked';
 
 const Home = () => {
-  const context=useContext(AiContext);
-  const {prompt, setprompt, image, setimage,resp}=context;
-  const [resps,setresps]=useState([]);
-  useEffect(()=>{
-    if(resp) setresps([...resps,{mess:resp, domain: 'mr-[40%]'}]);
-  },[resp])
-  useEffect(()=>{
-    if(resp) setresps([...resps,{mess:prompt, domain: 'ml-[40%]'}]);
-  },[prompt])
+  const context = useContext(AiContext);
+  const { prompt, setprompt, image, setimage, resp } = context;
+  const [resps, setresps] = useState([]);
+
+  useEffect(() => {
+    if (resp) setresps((prevResps) => [...prevResps, { mess: resp, domain: 'mr-[40%]' }]);
+  }, [resp]);
+
+  useEffect(() => {
+    if (prompt) setresps((prevResps) => [...prevResps, { mess: prompt, domain: 'ml-[40%]' }]);
+  }, [prompt]);
+
+  const renderer = new marked.Renderer();
+  renderer.code = (code, language) => {
+    return `<div class="code-block">${code}</div>`;
+  };
+
+  const renderCodeBlocks = (html) => {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(html, 'text/html');
+    const codeBlocks = doc.querySelectorAll('.code-block');
+
+    codeBlocks.forEach((block) => {
+      const code = block.innerHTML;
+      const highlightedCode = (
+        <SyntaxHighlighter style={dark} language={block.getAttribute('class')} children={code} />
+      );
+      block.parentNode.replaceChild(highlightedCode, block);
+    });
+
+    return { __html: doc.body.innerHTML };
+  };
+
   return (
     <div className='min-h-full overflow-y-auto max-h-screen w-screen'>
       <div className='mb-[20%] mt-[2%] w-full'>
         { 
-          resps.map((res,index)=>{
-            return(
+          resps.map((res, index) => {
+            const renderedHTML = marked(res.mess, { renderer });
+            return (
               <div
                 key={index}
-                className={`max-w-[40%] overflow-x-auto ml-auto mr-auto bg-zinc-700 mb-8 p-3 rounded-xl ${res.domain}`}
-                dangerouslySetInnerHTML={{ __html: res.mess }}
+                className={`max-w-[70%] overflow-x-auto ml-auto mr-auto bg-zinc-700 mb-8 p-3 rounded-xl ${res.domain}`}
+                dangerouslySetInnerHTML={renderCodeBlocks(renderedHTML)}
               />
-            )
+            );
           })
         }
       </div>
-      <Prompt/>
+      <Prompt />
     </div>
-  )
-}
+  );
+};
 
-export default Home
-
-
+export default Home;
