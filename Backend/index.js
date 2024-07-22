429390ade36272c257b493454291dfd5603fed85
Aditya Bansal <98232752+AdityaBansal1402@users.noreply.github.com>
2024-07-22 17:57:47 +0530

    work

--- Backend/index.js
+++ Backend/index.js
@@ -10,7 +10,7 @@ connectto();
 app.use(cors())
 app.use(express.json())
 app.use('/api/auth',require('./routes/auth'))
-// app.use('/api/notes',require('./routes/notes'))
+app.use('/api/prompts',require('./routes/prompt'))
 // app.use('/api/video',require('./routes/video'))
 // app.use('/api/comments',require('./routes/comments'))
 app.listen(port, ()=>{
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,10 @@
 import React, { useContext } from 'react'
 import InfoContext from '../Contexts/Info/InfoContext'
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
   const context1=useContext(InfoContext);
+  const history=useNavigate();
   const {info}=context1;
   return (
     <div className='sticky top-0 flex justify-between items-center p-2 pl-5 pr-5 bg-zinc-950 bg-opacity-70 backdrop-filter backdrop-blur-sm'>
@@ -11,9 +13,9 @@ const Navbar = () => {
       <img className='w-1/6 rounded-full' src={info.pic}  alt=" "></img>
       <div className='ml-2'> {info.name}</div>
       </div>
-      <div className='text-white'>
+      <button className='text-white'  onClick={()=>history(0)}>
       <span className="material-symbols-outlined">add_circle</span>
-      </div>
+      </button>
     </div>
   )
 }
